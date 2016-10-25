010b998858aab2475eedfe63c1412fbdda9bbc5e
Isaac Gongora <IsaacGongora@Usuarios-MacBook-Air.local>
2016-10-25 17:01:53 -0500

    Refactoring

--- UIDropDown/UIDropDown.swift
+++ UIDropDown/UIDropDown.swift
@@ -383,3 +383,68 @@ extension UIDropDown: UITableViewDelegate {
         privatedidSelect("\(self.options[indexPath.row])", selectedIndex!)
     }
 }
+
+// Arrow
+
+enum Position {
+    case left
+    case down
+    case right
+    case up
+}
+
+class Arrow: UIView {
+    
+    var position: Position = .down {
+        didSet{
+            switch position {
+            case .left:
+                self.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+                break
+                
+            case .down:
+                self.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2)
+                break
+                
+            case .right:
+                self.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+                break
+                
+            case .up:
+                self.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                break
+            }
+        }
+    }
+    
+    init(origin: CGPoint, size: CGFloat) {
+        super.init(frame: CGRect(x: origin.x, y: origin.y, width: size, height: size))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        // Get size
+        let size = self.layer.frame.width
+        
+        // Create path
+        let bezierPath = UIBezierPath()
+        
+        // Draw points
+        let qSize = size/4
+        
+        bezierPath.move(to: CGPoint(x: 0, y: qSize))
+        bezierPath.addLine(to: CGPoint(x: size, y: qSize))
+        bezierPath.addLine(to: CGPoint(x: size/2, y: qSize*3))
+        bezierPath.addLine(to: CGPoint(x: 0, y: qSize))
+        bezierPath.close()
+        
+        // Mask to path
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = bezierPath.cgPath
+        self.layer.mask = shapeLayer
+    }
+}
