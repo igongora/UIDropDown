231c9c6e4bfceaacfa3ff034d1f0de4036553c70
Isaac Gongora <igongoracastano@gmail.com>
2016-10-26 17:30:48 -0500

    Carthage files

---
+++ ExampleApp/FUIDropDown/FUIDropDown.h
@@ -0,0 +1,19 @@
+//
+//  FUIDropDown.h
+//  FUIDropDown
+//
+//  Created by Isaac Gongora on 10/26/16.
+//  Copyright © 2016 Isaac Gongora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FUIDropDown.
+FOUNDATION_EXPORT double FUIDropDownVersionNumber;
+
+//! Project version string for FUIDropDown.
+FOUNDATION_EXPORT const unsigned char FUIDropDownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FUIDropDown/PublicHeader.h>
+
+
