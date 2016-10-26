cade17071757dbd0ae630977a2aaba36050771f7
Isaac Gongora <igongoracastano@gmail.com>
2016-10-26 16:50:29 -0500

    Files to carthage added (Were blocked by .gitignore)

---
+++ ExampleApp/UIDropDownFramework/UIDropDownFramework.h
@@ -0,0 +1,19 @@
+//
+//  UIDropDownFramework.h
+//  UIDropDownFramework
+//
+//  Created by Isaac Gongora on 10/26/16.
+//  Copyright © 2016 Isaac Gongora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIDropDownFramework.
+FOUNDATION_EXPORT double UIDropDownFrameworkVersionNumber;
+
+//! Project version string for UIDropDownFramework.
+FOUNDATION_EXPORT const unsigned char UIDropDownFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIDropDownFramework/PublicHeader.h>
+
+
