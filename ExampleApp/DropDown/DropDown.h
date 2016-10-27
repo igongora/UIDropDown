975bef6daefa760ff47b8666967aa15d9e35e0d1
Isaac Gongora <igongoracastano@gmail.com>
2016-10-27 12:44:46 -0500

    "Podspec modified"

---
+++ ExampleApp/DropDown/DropDown.h
@@ -0,0 +1,19 @@
+//
+//  DropDown.h
+//  DropDown
+//
+//  Created by Isaac Gongora on 10/26/16.
+//  Copyright © 2016 Isaac Gongora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DropDown.
+FOUNDATION_EXPORT double DropDownVersionNumber;
+
+//! Project version string for DropDown.
+FOUNDATION_EXPORT const unsigned char DropDownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropDown/PublicHeader.h>
+
+
